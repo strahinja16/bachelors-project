5c425d9911bfbba06f3eb1f64485faf8ff025aab
Strahinja Laktovic <strahinja.laktovic@gmail.com>
2019-08-10 16:08:05 +0200

    Frontend: company

---
+++ bachelors-react/src/components/Company/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Col, Row } from 'react-flexbox-grid';
+import { FaPhone, FaEnvelope, FaMap } from 'react-icons/fa';
+import style from './styles.scss';
+
+const Company = () => (
+  <section className={style.company}>
+    <div className={style.container}>
+      <Row>
+        <Col className={style.col} xs={12} sm={12} md={12} lg={4}>
+          <h4>Contact Us</h4>
+          <ul>
+            <li><FaPhone /> (617) 555-5555</li>
+            <li><FaEnvelope /> support@apptheme.test</li>
+            <li><FaMap /> 400 Main st, New York NY</li>
+          </ul>
+        </Col>
+        <Col className={style.col} xs={12} sm={12} md={12} lg={4}>
+          <h4>About Us</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</p>
+        </Col>
+        <Col className={style.col} xs={12} sm={12} md={12} lg={4}>
+          <h4>Newsletter</h4>
+          <p>Lorem ipsum dolor sit amet</p>
+          <form>
+            <input type="text" name="email" placeholder="Enter Email" />
+            <button type="submit" name="button">Submit</button>
+          </form>
+        </Col>
+      </Row>
+    </div>
+  </section>
+);
+
+export default Company;
--- bachelors-react/src/pages/Dashboard/index.jsx
+++ bachelors-react/src/pages/Dashboard/index.jsx
@@ -3,12 +3,14 @@ import { withRouter } from 'react-router-dom';
 import Showcase from '../../components/Showcase';
 import Features from '../../components/Features';
 import Contact from '../../components/Contact';
+import Company from '../../components/Company';
 
 const Dashboard = () => (
   <Fragment>
     <Showcase />
     <Features />
     <Contact />
+    <Company />
   </Fragment>
 );
 
