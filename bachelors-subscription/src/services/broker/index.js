24d59ac8c7f740bbfe8ee61c3251c5fcb7de9c8e
Strahinja Laktovic <strahinja.laktovic@gmail.com>
2019-10-04 20:20:59 +0200

    Subscription service cleanup and npm packages installation

--- bachelors-subscription/src/services/broker/index.js
+++ bachelors-subscription/src/services/broker/index.js
@@ -14,6 +14,6 @@ const broker = new ServiceBroker({
   actions,
 });
 
-broker.start();
+// broker.start();
 
 module.exports = broker;
--- bachelors-subscriptiton/src/index.js
+++
@@ -1,8 +0,0 @@
-const initBroker = require('services/broker');
-
-module.exports = () => {
-  const broker = initBroker();
-  broker.start();
-
-  console.log('broker start');
-};
